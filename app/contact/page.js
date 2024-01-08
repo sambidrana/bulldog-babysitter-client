f7be442f8e351d585a473ddab59e318fe9eb470c
Sambid Rana <sambid.rana1@gmail.com>
2024-01-08 23:22:29 +1100

    mobile responsive

---
+++ app/contact/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactUs from '../(components)/ContactUs'
+
+const ContactPage = () => {
+  return (
+    <div className='mt-10'>
+        <ContactUs />
+    </div>
+  )
+}
+
+export default ContactPage
